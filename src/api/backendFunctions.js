83c35ba565c68f9b155b9384be3a5ecbd0dcc6c8
info <info@monkeebizznus.com>
2026-03-24 01:44:03 +0000

    Update app pages

--- src/api/backendFunctions.js
+++ src/api/backendFunctions.js
@@ -4,3 +4,4 @@ export const submitLead = base44.functions.submitLead;
 export const dealAccessSubmit = base44.functions.dealAccessSubmit;
 export const dealAccessSms2 = base44.functions.dealAccessSms2;
 export const dealAccessSms1 = base44.functions.dealAccessSms1;
+export const dealAccessSms1Batch = base44.functions.dealAccessSms1Batch;
