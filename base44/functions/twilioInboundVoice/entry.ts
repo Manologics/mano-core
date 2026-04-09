befe3347ee6b75321793bf5010cf41af446f6967
info <info@monkeebizznus.com>
2026-04-09 01:03:25 +0000

    File changes

--- base44/functions/twilioInboundVoice/entry.ts
+++ base44/functions/twilioInboundVoice/entry.ts
@@ -1,4 +1,8 @@
-Deno.serve(async (_req) => {
+Deno.serve(async (req) => {
+  if (req.method !== "GET" && req.method !== "POST") {
+    return new Response("Method Not Allowed", { status: 405 });
+  }
+
   return new Response(`<?xml version="1.0" encoding="UTF-8"?>
 <Response>
   <Say voice="alice">Thanks for calling Monkee Bizz. Please hold while we connect you.</Say>
