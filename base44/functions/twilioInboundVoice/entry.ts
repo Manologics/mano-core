d8ba8ec8cdeb3df34c83e7744eb7b9b2ee6111e2
info <info@monkeebizznus.com>
2026-04-09 00:56:53 +0000

    File changes

--- base44/functions/twilioInboundVoice/entry.ts
+++ base44/functions/twilioInboundVoice/entry.ts
@@ -1,15 +1,8 @@
 Deno.serve(async (_req) => {
-  return new Response(
-    `<?xml version="1.0" encoding="UTF-8"?>
+  return new Response(`<?xml version="1.0" encoding="UTF-8"?>
 <Response>
-  <Say>Monkee Bizz AI is online.</Say>
-  <Hangup/>
-</Response>`,
-    {
-      status: 200,
-      headers: {
-        "Content-Type": "text/xml"
-      }
-    }
-  );
+  <Say voice="alice">Thanks for calling Monkee Bizz. Please hold while we connect you.</Say>
+</Response>`, {
+    headers: { "Content-Type": "text/xml" }
+  });
 });
